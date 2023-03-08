3c4f94611b3421fbd70dcb28d0e0635565589da0
Luís Cláudio Prado <luisclaudio.praado@gmail.com>
2023-03-08 04:23:53 -0300

    feat: taskRepo implementation

--- src/infra/containers/index.ts
+++ src/infra/containers/index.ts
@@ -1,8 +1,14 @@
 import { container } from 'tsyringe'
 
+import { TaskRepository } from '../mongoose/repositories/TaskRepository'
 import { UserRepository } from '../mongoose/repositories/UserRepository'
 
 container.registerSingleton(
     'UserRepository',
     UserRepository
+)
+
+container.registerSingleton(
+    'TaskRepository',
+    TaskRepository
 )
